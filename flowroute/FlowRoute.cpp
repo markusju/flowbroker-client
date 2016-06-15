1a7a09f908d7a0a5d09ef14b8c9b81b1eac54185
Markus Jungbluth <markusju@users.noreply.github.com>
2016-06-15 08:19:04 +0000

    Gepointert

--- flowroute/FlowRoute.cpp
+++ flowroute/FlowRoute.cpp
@@ -11,7 +11,7 @@ FlowRoute::FlowRoute() {
 }
 
 string FlowRoute::toString() const {
-    return this->parseToRequest().toString();
+    return this->parseToRequest()->toString();
 }
 
 
@@ -22,92 +22,92 @@ void FlowRoute::parsePreChecks() const {
 }
 
 
-void FlowRoute::parsePostChecks(BrokerRequest& request) const {
+void FlowRoute::parsePostChecks(BrokerRequest* request) const {
     //FUTURE USE...
     //TODO
 }
 
-BrokerRequest FlowRoute::parseToRequest() const {
+BrokerRequest* FlowRoute::parseToRequest() const {
     this->parsePreChecks();
 
-    BrokerRequest request(this->filter_action->getMethod());
+    BrokerRequest* request = new BrokerRequest(this->filter_action->getMethod());
 
     for (auto el : this->filter_action->getMethodArguments()) {
-        request.addRequestArgument(el);
+        request->addRequestArgument(el);
     }
 
     if (this->destination_address != NULL) {
-        request.addParameter(
+        request->addParameter(
                 this->destination_address->getKey(),
                 this->destination_address->getValue()
         );
     }
 
     if (this->protocol != NULL) {
-        request.addParameter(
+        request->addParameter(
                 this->protocol->getKey(),
                 this->protocol->getValue()
         );
     }
 
     if (this->port != NULL) {
-        request.addParameter(
+        request->addParameter(
                 this->port->getKey(),
                 this->port->getValue()
         );
     }
 
     if (this->destination_port != NULL) {
-        request.addParameter(
+        request->addParameter(
                 this->destination_port->getKey(),
                 this->destination_port->getValue()
         );
     }
 
     if (this->source_port != NULL) {
-        request.addParameter(
+        request->addParameter(
                 this->source_port->getKey(),
                 this->source_port->getValue()
         );
     }
 
     if (this->icmp_type != NULL) {
-        request.addParameter(
+        request->addParameter(
                 this->icmp_type->getKey(),
                 this->icmp_type->getValue()
         );
     }
 
     if (this->icmp_code != NULL) {
-        request.addParameter(
+        request->addParameter(
                 this->icmp_code->getKey(),
                 this->icmp_code->getValue()
         );
     }
 
     if (this->tcp_flags != NULL) {
-        request.addParameter(
+        request->addParameter(
                 this->tcp_flags->getKey(),
                 this->tcp_flags->getValue()
         );
     }
 
     if (this->packet_length != NULL) {
-        request.addParameter(
+        request->addParameter(
                 this->packet_length->getKey(),
                 this->packet_length->getValue()
         );
     }
 
     if (this->dscp != NULL) {
-        request.addParameter(
+        request->addParameter(
                 this->dscp->getKey(),
                 this->dscp->getValue()
         );
     }
 
     if (this->fragment != NULL) {
-        request.addParameter(
+        request->addParameter(
                 this->fragment->getKey(),
                 this->fragment->getValue()
         );
